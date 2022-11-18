2914e03de0841dbba04fcf9b90d3751d48ccdaf6
Aditya Mahamuni <adimahamuni25@gmail.com>
2022-11-18 13:00:24 +0530

    Updated Navbar in Header.jsx

--- disney-hotstar-clone/src/components/Header.jsx
+++ disney-hotstar-clone/src/components/Header.jsx
@@ -7,8 +7,32 @@ const Header = (props) => {
                 <img src="/images/logo.svg" alt="Disney+ Hotstar" />
             </Logo>
             <NavMenu>
-                Menu
+                <a href='/home'>
+                    <img src="/images/home-icon.svg" alt="Home" />
+                    <span>HOME</span>
+                </a>
+                <a href='/search'>
+                    <img src="/images/search-icon.svg" alt="Search" />
+                    <span>SEARCH</span>
+                </a>
+                <a href='/watchlist'>
+                    <img src="/images/watchlist-icon.svg" alt="Watchlist" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href='/originals'>
+                    <img src="/images/original-icon.svg" alt="Home" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href='/movies'>
+                    <img src="/images/movie-icon.svg" alt="Home" />
+                    <span>MOVIES</span>
+                </a>
+                <a href='/series'>
+                    <img src="/images/series-icon.svg" alt="Series" />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
+            <Login>Login</Login>
         </Nav>
     )
 }
@@ -24,7 +48,7 @@ const Nav = styled.nav`
     justify-content: space-between;
     align-items: center;
     padding: 0 36px;
-    letter-spacing: 16px; // WTF?
+    letter-spacing: 16px;
     z-index: 3;
 `;
 
@@ -55,8 +79,76 @@ const NavMenu = styled.div`
     margin-right: auto;
     margin-left: 25px;
 
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+            z-index: auto;
+        }
+
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            margin-left: 2px;
+            padding: 2px 0px;
+            white-space: nowrap;
+            position: relative;
+
+            &:before {
+                background-color: rgb(249, 249, 249);
+                border-radius: 0px 0px 4px 4px;
+                bottom: -2px;
+                content: "";
+                height: 2px;
+                left: 0px;
+                opacity: 0;
+                position: absolute;
+                right: 0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+
+        &:hover {
+            span:before {
+                transform: scaleX(1);
+                visibility: visible;
+                opacity: 1 !important;
+            }
+        }
+    }
+
     @media only screen and (max-width: 768px) {
         display: none;
+    }
+`;
+
+const Login = styled.a`
+    background-color: rgba(0,0,0,0.6);
+    padding: 8px 16px;
+    font-size: 14px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: all 0.2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #090b13;
+        font-weight: bold;
+        border-color: transparent;
     }
 `;
 
