a736cef51443d2d59190754c4fa108928bb47cc8
Aditya Mahamuni <adimahamuni25@gmail.com>
2022-11-19 12:04:24 +0530

    Minute updates

--- disney-hotstar-clone/src/index.js
+++ disney-hotstar-clone/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Provider from "./Provider";
-import store from "./app/store";
+// import Provider from "./Provider";
+// import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
